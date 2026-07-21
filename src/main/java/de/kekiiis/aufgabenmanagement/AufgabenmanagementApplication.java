9a77a5eea28d1afe2900fed583a5777ea284c1d4
Kekiiis <kekis.usestheinternet@gmail.com>
2026-07-21 15:44:48 +0200

    Add files via upload

---
+++ src/main/java/de/kekiiis/aufgabenmanagement/AufgabenmanagementApplication.java
@@ -0,0 +1,13 @@
+package de.kekiiis.aufgabenmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AufgabenmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AufgabenmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/kekiiis/aufgabenmanagement/AufgabenmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kekiiis.aufgabenmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AufgabenmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
